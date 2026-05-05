7a93782b9a017ac91a35942dc16c3ac83218de10
Ackin.Dev <ackinlino123@gmail.com>
2026-05-04 23:05:51 -0300

    Add to store new expense screen (just visual)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,63 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+interface ExpenseScreen {
+    void mainExpenseMenu(Scanner scan);
+    void addExpense(Scanner scan);
+//    void updateExpense(String expense, String desc, double amount);
+//    void viewAllExpenses();
+}
+
+public class Main implements ExpenseScreen {
+
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Main app = new Main();
+        app.mainExpenseMenu(scan);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    @Override
+    public void mainExpenseMenu(Scanner scan) {
+        while(true) {
+            System.out.println("\nWelcome to the Expense Tracker CLI!!!");
+            System.out.println("1. Add a new expense");
+            System.out.println("2. Update a expense");
+            System.out.println("3. Delete an expense");
+            System.out.println("4. View all expenses");
+            System.out.println("5. View summary of all expenses");
+            System.out.println("6. View summary expenses for a specific month (of current year)");
+            System.out.println("7. Exit");
+            System.out.print("Select an option: ");
+            int option = scan.nextInt();
+
+            switch (option) {
+                case 1:
+                    addExpense(scan);
+                    break;
+                case 7:
+                    System.out.println("Quitting...");
+                    return;
+                default:
+                    System.out.println("Invalid option!! Try Again...");
+            }
+
+        }
+
+    }
+
+    @Override
+    public void addExpense(Scanner scan) {
+        while(true) {
+            System.out.print("Expense: ");
+            scan.next();
+            System.out.print("Description: ");
+            scan.next();
+            System.out.print("Amount: ");
+            scan.nextDouble();
+            System.out.println("New expense!!!");
+            return;
         }
     }
+
 }
